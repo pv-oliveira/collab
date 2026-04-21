a38a362064bc5c3b317ce592a8aa1fb75f35b2a4
Paulo Victor <paulo.victor.ig@hotmail.com>
2026-04-21 12:13:45 -0300

    feat: implement JWT token generation for authenticated users

---
+++ internal/middleware/jwt.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(secret, userID string) (string, error) {
+	claims := jwt.MapClaims{
+		"sub": userID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
