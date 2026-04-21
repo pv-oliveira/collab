59b16f2d84ea33336aeac5a6b2d888016289a36b
Paulo Victor <paulo.victor.ig@hotmail.com>
2026-04-21 13:47:42 -0300

    fix: misspelling

--- internal/models/document.go
+++ internal/models/document.go
@@ -4,6 +4,7 @@ import "time"
 
 type Document struct {
 	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
